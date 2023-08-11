dc89ca528ae16b931017df42a8cafada40cb1396
Mercyssh <ima.omrane@gmail.com>
2023-08-11 08:53:32 +0530

    Minor Refactor in UIUpdate.cs.
    Renamed function UpdateUI(), broke down into several functions, Showall, Hideall, Showmanager, etc..
    
    UpdateUI now stores a local copy of Response class, instead of taking input parameter within the function.
    
    Added Basic DOTween to ListItem class. Fades in on start, and Pops out when getting deleted.

--- Assets/Task1/Scripts/ListItem.cs
+++ Assets/Task1/Scripts/ListItem.cs
@@ -2,21 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class ListItem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("References")]
     public Text LabelObject;
     public Text PointsObject;
 
-    void Start()
+    [Header("Animation Settings")]
+    public float fadeInDuration = .5f;
+    public float popOutDuration = .5f;
+    public float delay = .2f;
+
+    private void Start()
     {
-        
+        FadeIn();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FadeIn()
     {
-        
+        if (TryGetComponent(out CanvasGroup canvasGroup))
+        {
+            canvasGroup.alpha = 0;
+            canvasGroup.DOFade(1, .5f).SetDelay(delay);
+        }
+
+    }
+
+    //Call when destroying listItem
+    public void PopOut()
+    {
+        if (TryGetComponent(out RectTransform rectTransform))
+        {
+            //We want the width to remain the same, but the height to transition to 0
+            Vector2 target = GetComponent<RectTransform>().sizeDelta;
+            target.y = 0;
+
+            rectTransform.DOSizeDelta(target, popOutDuration).OnComplete(()=> { 
+                if(gameObject!=null)
+                    Destroy(gameObject); 
+            });
+        }
+    }
+
+    //This function is called when instantiating,
+    //Each item has incrementally more delay, makes the animation to look more polished
+    public void SetDelay(float d)
+    {
+        delay = d;
     }
 }
--- Assets/Task1/Scripts/UIUpdater.cs
+++ Assets/Task1/Scripts/UIUpdater.cs
@@ -1,35 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UIUpdater : MonoBehaviour
 {
-    public GameObject list;
+    //Reference to the container of list
+    public GameObject listContainer;
+
+    //We store a local reference to the retrieved data.
+    [HideInInspector]
+    public Response response;
 
     //This is a reference to a prefab of a list Item
     public GameObject template;
 
-    public void UpdateUI(Response response)
+    public void ShowAll()
     {
+        HideAll();
+
         // Again, since 'Data' class is actually a sequence of manually defined variables
         // We need to manually call the function to return them as an array in a predefined order
         // This would not have to be done if the data object in JSON was presented as an Array, OR 
-        // External libraries were allowed to be used.
+        // if External libraries were allowed to be used.
         ClientData[] dataArray = response.data.AsArray();
 
         int index = 0;
         foreach (Client client in response.clients)
         {
-            GameObject newItem = Instantiate(template, list.transform);
-            newItem.GetComponent<ListItem>().LabelObject.text = "Label : "+client.label;
+            GameObject newItem = Instantiate(template, listContainer.transform);
+            ListItem listItem = newItem.GetComponent<ListItem>();
+
+            //Update Label
+            listItem.LabelObject.text = "Label : "+client.label;
 
             //Find appropriate points value for each item
             string pointsValue = "???";
             if (index < dataArray.Length)
                 pointsValue = dataArray[index].points.ToString();
-            newItem.GetComponent<ListItem>().PointsObject.text = "Points : " + pointsValue;
+            listItem.PointsObject.text = "Points : " + pointsValue;
+
+            //Set animation Delay
+            listItem.SetDelay(index * listItem.delay);
 
             index++;
         }
     }
+
+    private void HideAll()
+    {
+        foreach(Transform child in listContainer.transform)
+        {
+            child.GetComponent<ListItem>().PopOut();
+        }
+    }
+
+    private void ShowManagers()
+    {
+        HideAll();
+    }
+
+    private void ShowNonManager()
+    {
+        HideAll();
+    }
+
 }
